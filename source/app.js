35949fb7fb224e5540554154f75f08add7d87e11
cjurden <colejurden@gmail.com>
2015-12-28 12:01:37 -0600

    first commit

---
+++ source/app.js
@@ -0,0 +1,5 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Application = require('./components/Application.react');
+
+ReactDOM.render(<h1>Hello World!</h1>, document.getElementById('react-app'));
